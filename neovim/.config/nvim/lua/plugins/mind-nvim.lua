7850f7da4cc98c37f6280fffe88cfd603cb44d50
Christian Kampka <christian@kampka.net>
2023-01-02 20:28:03 +0100

    [neovim] Add autocommit for mind

--- neovim/.config/nvim/lua/plugins/mind-nvim.lua
+++ neovim/.config/nvim/lua/plugins/mind-nvim.lua
@@ -1,11 +1,21 @@
 local M = {
-  'phaazon/mind.nvim',
-  branch = 'v2.2',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  cmd = 'MindOpenMain',
-  config = function()
-    require('mind').setup()
-  end
+	"phaazon/mind.nvim",
+	branch = "v2.2",
+	dependencies = {
+		"declancm/git-scripts.nvim",
+		"nvim-lua/plenary.nvim",
+	},
+	cmd = "MindOpenMain",
+	config = function()
+		local mind = require("mind")
+		mind.setup()
+
+		local git = require("git-scripts")
+		git.setup({
+			commit_on_save = true,
+			warnings = false,
+		})
+	end,
 }
 
 return M
