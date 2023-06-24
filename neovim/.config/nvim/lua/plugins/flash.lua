3b704750ee8a824c4bd5ad098a89afe13244584f
Christian Kampka <christian@kampka.net>
2023-06-24 10:26:44 +0200

    [neovim] Replace leap with flash

---
+++ neovim/.config/nvim/lua/plugins/flash.lua
@@ -0,0 +1,24 @@
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  opts = {},
+  keys = {
+    {
+      "s",
+      mode = { "n", "x", "o" },
+      function()
+        -- default options: exact mode, multi window, all directions, with a backdrop
+        require("flash").jump()
+      end,
+      desc = "Flash",
+    },
+    {
+      "S",
+      mode = { "n", "o", "x" },
+      function()
+        require("flash").treesitter()
+      end,
+      desc = "Flash Treesitter",
+    },
+  },
+}
