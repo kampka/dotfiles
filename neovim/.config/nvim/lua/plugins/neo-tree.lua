ebfdc929f8e3b9471f709700c34a673087dc7297
Christian Kampka <christian@kampka.net>
2024-05-03 17:14:57 +0200

    [neovim] Upgrade neotree to v3

--- neovim/.config/nvim/lua/plugins/neo-tree.lua
+++ neovim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,32 +1,32 @@
-
 local M = {
-  "nvim-neo-tree/neo-tree.nvim",
-  dependencies = { 
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  init = function()
-    vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
-  end,
-  cmd = "Neotree",
-  keys = {
-    {
-      "<leader>ft",
-      function()
-        require("neo-tree.command").execute({
-          toggle = true,
-        })
-      end,
-      desc = "NeoTree",
-    },
-  },
-  config = {
-    filesystem = {
-      follow_current_file = true,
-      hijack_netrw_behavior = "open_current",
-    },
-  },
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	init = function()
+		vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+	end,
+	cmd = "Neotree",
+	keys = {
+		{
+			"<leader>ft",
+			function()
+				require("neo-tree.command").execute({
+					toggle = true,
+				})
+			end,
+			desc = "NeoTree",
+		},
+	},
+	config = {
+		filesystem = {
+			follow_current_file = true,
+			hijack_netrw_behavior = "open_current",
+		},
+	},
 }
 
 return M
