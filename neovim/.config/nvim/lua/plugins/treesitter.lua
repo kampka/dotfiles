aa3aa8beaf60eb1627beaac95528ffefb776e03a
Christian Kampka <christian@kampka.net>
2024-05-03 17:15:51 +0200

    [neovim] Use C compiler provided through NixOS config for treesitter parsers

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -3,33 +3,28 @@
 -- for eg. more accurate syntax highlighting
 local M = {
 	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
 	event = "BufReadPost",
 	config = function()
-		-- Use zig as the default compilers, it has a stable static compile chain
-		require("nvim-treesitter.install").compilers = { "zig" }
-		require("nvim-treesitter.configs").setup({
-			query_linter = {
-				enable = true,
-				use_virtual_text = true,
-				lint_events = { "BufWrite", "CursorHold" },
-			},
-			ensure_installed = {},
-			ignore_install = {
-				"norg", -- norg is listed as installed but fails to build with zig atm
-				"fennel", -- norg is listed as installed but fails to build with zig atm
-			},
-			highlight = {
-				enable = true,
-				--disable = { 'scala' },
-			},
-			indent = {
-				enable = true,
-			},
-			autopairs = {
-				enable = true,
-			},
-		})
+		require("nvim-treesitter.install").compilers =
+			{
+				vim.env.NIX_TREESITTER_COMPILER,
+			}, require("nvim-treesitter.configs").setup({
+				query_linter = {
+					enable = true,
+					use_virtual_text = true,
+					lint_events = { "BufWrite", "CursorHold" },
+				},
+				highlight = {
+					enable = true,
+					--disable = { 'scala' },
+				},
+				indent = {
+					enable = true,
+				},
+				autopairs = {
+					enable = true,
+				},
+			})
 	end,
 }
 
